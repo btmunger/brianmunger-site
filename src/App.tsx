bd6ae48fe0bf3d4629a86bbce5ac7360b2cd01dc
btmunger <btmunger@gmail.com>
2026-02-03 20:06:16 -0800

    feat: fixed styling issue, content takes entire page now

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
 
 function App() {
+
   return (
     <>
-      <h1>Brian Munger's Website</h1>
+      {/* Navigation Bar */}
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+
+      {/* Main Content */}
+      <header id="home" className="hero">
+        <h1>Brian Munger's Website</h1>
+      </header>
     </>
   )
 }
